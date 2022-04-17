a786609f9291a9642a2fab8821687479c6ae5797
Wilson Jonathan Oey <61810752+wilsonoey60@users.noreply.github.com>
2022-04-17 12:50:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const PORT = 8000
 const HOST = '0.0.0.0'
 
 const app = express()
+
+app.use(express.static(__dirname + '/WebProfile.html'));
+
 app.get('/', (req, res) => {
-    res.sendFile('./WebProfile.html')
+    res.sendFile(__dirname + '/WebProfile.html')
 })
 
 app.listen(PORT, HOST)
